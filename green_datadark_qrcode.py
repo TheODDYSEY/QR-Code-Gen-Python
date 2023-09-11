b27bfb0cd6668bb671829fd46dfe79ef77b4b30f
TheODDYSEY <mosessabz07@gmail.com>
2023-09-11 22:30:43 +0300

    Identified data modules

--- green_datadark_qrcode.py
+++ green_datadark_qrcode.py
@@ -2,10 +2,11 @@ import segno
 
 qrcode = segno.make_qr("Hello World")
 qrcode.save(
-    "green_datadark_qrcode.png",
+    "green_datamodules_qrcode.png",
     scale=5,
-    light="lightblue"
+    light="lightblue",
     dark="darkblue",
     data_dark="green",
+    data_light="lightgreen",
     
 )
