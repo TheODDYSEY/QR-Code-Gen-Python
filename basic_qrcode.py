d2458441248c9a23734d48d32e1b5833e4707bf6
TheODDYSEY <mosessabz07@gmail.com>
2023-09-11 22:25:29 +0300

    Created new py script

--- basic_qrcode.py
+++ basic_qrcode.py
@@ -3,6 +3,7 @@ import segno
 qrcode = segno.make_qr("Hello ,World")
 qrcode.save("darkblue_qrcode.png",
             scale=5,
-            light= "darkblue"
+            light= "darkblue",
+            quiet_zone="maroon",
             
             )
---
+++ green_datadark_qrcode.py
@@ -0,0 +1,11 @@
+import segno
+
+qrcode = segno.make_qr("Hello World")
+qrcode.save(
+    "green_datadark_qrcode.png",
+    scale=5,
+    light="lightblue"
+    dark="darkblue",
+    data_dark="green",
+    
+)
