b20fcdc8ee744e0263b17c12ddb5f2f2b9a0c2e6
TheODDYSEY <mosessabz07@gmail.com>
2023-09-11 23:08:22 +0300

    fin with manip of x y axis

--- rotate/rotate_qrcode.py
+++ rotate/rotate_qrcode.py
@@ -2,5 +2,10 @@ import segno
 
 qrcode = segno.make_qr("Hello,World")
 
-qrcode_rotated = qrcode.to_pil().rotate(45)
-qrcode_rotated.save("rotated_qrcode.png")
+qrcode_rotated = qrcode.to_pil(
+    scale=5,
+    light="lightblue",
+    dark="green",
+    ).rotate(45, expand=True)
+
+qrcode_rotated.save("rotated_qrcode3.png",)
